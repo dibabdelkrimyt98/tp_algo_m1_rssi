f930cd87af860ee2319a2572db504f3bdcc7726b
dibabdelkrimyt98 <dibabdelkrimyt98@gmail.com>
2025-10-26 15:06:30 +0800

    finale part of TP 01

--- Fibonacci3.java
+++ Fibonacci3.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 public class Fibonacci3 {
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+    public static int fibonacci(int n, int a, int b) {
+
+        if (n == 0)
+            return a;
+        
+        if (n == 1) 
+            return b;
+        return fibonacci(n - 1, b, a + b);
     }
 
     public static void main(String[] args) {
@@ -14,7 +17,8 @@ public class Fibonacci3 {
             int n = scanner.nextInt();
             scanner.close();
             long tempsDebut = System.currentTimeMillis();
-            System.out.println(" Le dernier terme est : " + fibonacci(n) + " :) ");
+            int resultat = fibonacci(n, 1, 1);
+            System.out.println(" Le dernier terme est : " + resultat + " :) ");
 
             long tempsFin = System.currentTimeMillis();
 
